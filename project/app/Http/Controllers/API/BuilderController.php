3b38e7c0b396e2f8a5e5bca4dc6091dab129d357
saitama3020 <27062041+saitama3020@users.noreply.github.com>
2019-04-18 15:34:26 +0530

    Added Builder Controller

--- project/app/Http/Controllers/API/BuilderController.php
+++ project/app/Http/Controllers/API/BuilderController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Builder;
+
 
 class BuilderController extends Controller
 {
@@ -11,10 +13,26 @@ class BuilderController extends Controller
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
+     * 
+     * @SWG\Get(
+     * path="/api/builders",
+     * tags={"Builders"},
+     * summary="List Builders",
+     * @SWG\Response(
+     * response="200",
+     * description="Success: List all Builders",
+     * @SWG\Schema(ref="#/definitions/Builder"),
+     * ),
+     * @SWG\Response(
+     * response="404",
+     * description="Not found"
+     * ),
+     * )
      */
     public function index()
     {
-        //
+        $listBuilder = Builder::all();
+        return $listBuilder;
     }
 
     /**
@@ -22,17 +40,51 @@ class BuilderController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
+     * 
+     * @SWG\Post(
+     * path="/api/builders",
+     * tags={"Builders"},
+     * summary="Create Builder",
+     * @SWG\Parameter(
+     * name="body",
+     * in="body",
+     * required="true",
+     * @SWG\Schema(ref="#/definitions/Builder"),
+     * description="Json Format"
+     * ),
+     * @SWG\Response(
+     * response="201",
+     * description="Success: A Newly Created Builder",
+     * @SWG\Schema(ref="#/definitions/Builder"),
+     * ),
+     * @SWG\Response(
+     * response="422",
+     * description="Missing mandatory field",
+     * ),
+     * @SWG\Response(
+     * response="404",
+     * description="Not found"
+     * ),
+     * @SWG\Response(
+     * response="405",
+     * description="Invalid HTTP method")
+     * )
      */
     public function store(Request $request)
     {
-        //
+        $createBuilder = Builder::create($request->all());
+        return $createBuilder;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int  $id\Get(
+     * path="/api/builders/{id}",
+     * )
      * @return \Illuminate\Http\Response
+     * 
+     * @SWG
      */
     public function show($id)
     {
